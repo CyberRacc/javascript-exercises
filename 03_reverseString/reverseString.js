05f2411b0a2c8958c539d97fd39a312a39f82446
CyberRacc <89751580+CyberRacc@users.noreply.github.com>
2023-06-27 22:41:23 +0100

    Started working on exercise 3.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,19 @@
 const reverseString = function() {
+    // I think I need to convert the word into an array.
+    // Create a new string from the array in reverse order somehow, review notes.
+    // Needs to use string methods .split() and .join()
+
+    splitString = String.spit();
+
+    const stringArray = [splitString];
+
+
+
+    const reversedArray = stringArray.reverse();
+
+    let reversedString = reversedArray.join();
+
+    return reversedString;
 
 };
 
