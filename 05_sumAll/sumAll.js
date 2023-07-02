b4f80696161707a2047fb347b5ff8be023c502c9
CyberRacc <89751580+CyberRacc@users.noreply.github.com>
2023-07-02 10:54:49 +0100

    Passed all tests, completed sumAll exercise.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -6,15 +6,17 @@ const sumAll = function(num1, num2) {
 
     let finalSum = 0;
 
-    // Need to use a for loop.
-    if (typeof num1  !== "number" || typeof num2 !== "number") {
-        return "ERROR | Not a number!"
+    // Used if condition to check if num1, num2 are numbers.
+    // Also to check if num1, num2 are negative.
+    if (typeof num1  !== "number" || typeof num2 !== "number" || num1 < 0 || num2 < 0) {
+        return "ERROR"
     } else {
         for (let i = min; i <= max; i++) {
 
             // This line is equivalent to finalSum = finalSum + i;
             finalSum += i;
             // It is simply a more concise way of writing it.
+
         }
         return finalSum;
     }
