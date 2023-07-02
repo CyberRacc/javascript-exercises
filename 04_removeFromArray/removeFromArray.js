8a0e4fe34efc4f9b80ada6eb068e2253ae443487
CyberRacc <89751580+CyberRacc@users.noreply.github.com>
2023-07-02 10:55:18 +0100

    Updated removeFromArray.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,9 +1,21 @@
 const removeFromArray = function(array, ...itemsToRemove) {
 
+    // Don't really understand what this code is doing, need to add
+    // explanations.
+    const newArray = [];
+    array.forEach(item => {
+        if (!itemsToRemove.includes(item)) {
+            newArray.push(item);
+        }
+    });
+
+    return newArray;
+
     // Simplified but I do not understand it, need explanation.
-    return array.filter(element => !itemsToRemove.includes(element));
+    //return array.filter(element => !itemsToRemove.includes(element));
 
 };
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
