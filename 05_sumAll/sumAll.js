f3246408849a050c274dc6ddc64e5676426a6b98
CyberRacc <89751580+CyberRacc@users.noreply.github.com>
2023-07-02 10:44:10 +0100

    Passed the first test of 05_sumAll exercise.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
 
+    // This defines two variables 
+    let min = Math.min(num1, num2);
+    let max = Math.max(num1, num2);
+
+    let finalSum = 0;
+
+    // Need to use a for loop.
+    if (typeof num1  !== "number" || typeof num2 !== "number") {
+        return "ERROR | Not a number!"
+    } else {
+        for (let i = min; i <= max; i++) {
+
+            // This line is equivalent to finalSum = finalSum + i;
+            finalSum += i;
+            // It is simply a more concise way of writing it.
+        }
+        return finalSum;
+    }
+  
 };
 
 // Do not edit below this line
