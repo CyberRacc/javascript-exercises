19c5cfe2930dfe1ca803e4a0096af3ebb35b6fad
CyberRacc <89751580+CyberRacc@users.noreply.github.com>
2023-07-11 23:19:41 +0100

    Started working on exercise 12_findTheOldest.

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,16 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    return people.reduce((oldest, currentPerson) => {
+        return 
+    })
 };
+
+// Read notes on objects and reduce().
+// Need to actuall understand it not just copying AI guide.
+
+const multiply = function(...args) {
+    return args.reduce((total, currentValue) => total * currentValue, 1)
+};
+  
 
 // Do not edit below this line
 module.exports = findTheOldest;
