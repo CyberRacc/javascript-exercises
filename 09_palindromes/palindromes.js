963d6ed71a6eb3ac89ccc5c2ec51899c11fa3cbb
CyberRacc <89751580+CyberRacc@users.noreply.github.com>
2023-07-11 21:57:40 +0100

    Finished 09 and 10 exercises.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,20 @@
-const palindromes = function () {
+const palindromes = function (string) {
 
+    let stringLower = string.toLowerCase();
+
+    let cleanedString = stringLower.replace(/[^a-z0-9]/g, '');
+
+    const stringArray = cleanedString.split("");
+
+    const reversedArray = stringArray.reverse();
+
+    let reversedString = reversedArray.join("");
+
+    if (cleanedString === reversedString) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
 
+    if (n < 0) {
+        return "OOPS";
+    }
+
+    let a = 0;
+    let b = 1;
+
+    for (i = 2; i <= n; i++) {
+        let temp = a;
+        a = b;
+        b = temp + b;
+    }
+    return b;
 };
 
 // Do not edit below this line
